3382a837048ae3c649aa0dc91fcc4c07e53dfd6a
FisanSyafa <fisansyafa812@gmail.com>
2025-07-04 19:58:34 +0700

    koneksi_login.php

--- admin/koneksi_login.php
+++ admin/koneksi_login.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$admin_name = "root";  // Ganti dengan username database Anda
-$admin_password = "";  // Ganti dengan password database Anda
-$dbname = "triptrove";
+$admin_name = "trip_fisan";  // Ganti dengan username database Anda
+$admin_password = "Databasefisan";  // Ganti dengan password database Anda
+$dbname = "trip_triptrove";
 
 // Create connection
 $conn = new mysqli($servername, $admin_name, $admin_password, $dbname);
