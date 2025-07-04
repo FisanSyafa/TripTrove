4a0fb5fb28fef2ef44cb4e9bfb1c42d8f72c358f
FisanSyafa <fisansyafa812@gmail.com>
2025-07-04 20:47:32 +0700

    delete_paket.php

--- admin/paket/delete_paket.php
+++ admin/paket/delete_paket.php
@@ -8,18 +8,26 @@ if(!isset($_SESSION['admin_name'])){
 
 $admin_name = $_SESSION['admin_name'];
 
-$id = isset($_GET['id']) ? $_GET['id'] : '';
+// Tangkap nama paket dari parameter URL
+$nama = isset($_GET['nama_paket_trip']) ? urldecode($_GET['nama_paket_trip']) : '';
 
-if ($id) {
-    $sql = "DELETE FROM paket_trip  WHERE id='$id'";
-    if (mysqli_query($link, $sql)) {
-        echo "Record deleted successfully";
+if ($nama) {
+    // Hindari SQL injection dengan prepared statement
+    $stmt = mysqli_prepare($link, "DELETE FROM paket_trip WHERE nama_paket_trip = ?");
+    mysqli_stmt_bind_param($stmt, "s", $nama);
+
+    if (mysqli_stmt_execute($stmt)) {
+        // Sukses
     } else {
-        echo "Error: " . $sql . "<br>" . mysqli_error($link);
+        echo "Error: " . mysqli_error($link);
+        exit();
     }
+
+    mysqli_stmt_close($stmt);
 }
 
 mysqli_close($link);
 header("Location: tampil_paket.php");
 exit();
 ?>
+
